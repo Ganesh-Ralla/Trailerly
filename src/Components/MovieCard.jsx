979654864102ad978a9c44a8924799d8678c7ea0
Ganesh Ralla <ganeshrtist@gmail.com>
2026-04-03 13:46:21 +0530

    clicks added

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ movies, type }) => {
         return (
             <div className=' mt-4 flex items-end gap-24 overflow-x-auto no-scrollbar overflow-y-hidden'>
                 {movies?.slice(0,10)?.map((movie, index) => (
-                    <div className='relative flex items-end min-w-37.5 md:min-w-45' key={movie.id}>
+                    <div onClick={() => { navigate(`/details/${movie.id}`) }} className='relative flex items-end min-w-37.5 md:min-w-45' key={movie.id}>
                         <p className='text-[200px] md:text-[240px] font-extrabold leading-none opacity-40'>{index + 1}</p>
                         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt=""
                             className='absolute bottom-0 left-16 md:left-20 lg:left-22 w-32 md:w-40 rounded-2xl'/>
