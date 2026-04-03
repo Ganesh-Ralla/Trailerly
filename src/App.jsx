b499d5a16b8b50c8e266552673d211b15a6a1685
Ganesh Ralla <ganeshrtist@gmail.com>
2026-04-03 12:31:36 +0530

    scroll issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,27 @@ import SearchPage from './Pages/SearchPage'
 import Favourites from './Pages/Favourites'
 import Details from './Pages/Details'
 import About from './Pages/About'
+import ScrollToTop from './Components/ScrollToTop'
 
 function App() {
   const [openSearchBar, setOpenSearchBar] = useState(false)
-  const [searchInput,setSearchInput] = useState("")
-  const [searchTaken,setSearchTaken] = useState(false)
+  const [searchInput, setSearchInput] = useState("")
+  const [searchTaken, setSearchTaken] = useState(false)
 
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
+
         <Navbar openSearchBar={openSearchBar} setOpenSearchBar={setOpenSearchBar}
-                searchInput={searchInput} setSearchInput={setSearchInput}
-                searchTaken={searchTaken} setSearchTaken={setSearchTaken} />
+          searchInput={searchInput} setSearchInput={setSearchInput}
+          searchTaken={searchTaken} setSearchTaken={setSearchTaken} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/search' element={<SearchPage searchInput={searchInput} searchTaken={searchTaken} />} />
-          <Route path='/favourite' element={<Favourites/>} />
-          <Route path='/details/:id' element={<Details/>} />
-          <Route path='/about' element={<About/>} />
+          <Route path='/favourite' element={<Favourites />} />
+          <Route path='/details/:id' element={<Details />} />
+          <Route path='/about' element={<About />} />
         </Routes>
         <BottomNav openSearchBar={openSearchBar} setOpenSearchBar={setOpenSearchBar} />
         <Footer />
